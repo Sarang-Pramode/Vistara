6bd04840cbf9c4f103f7f8d83eaf0ec767c868e9
Sarang-Pramode <sarangpramode1997@gmail.com>
2023-04-15 05:24:47 -0400

    shifted to analyzing central park

--- pages/3_Roosevelt_Island.py
+++ pages/3_Roosevelt_Island.py
@@ -2,6 +2,13 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.express as px
+from shapely.ops import unary_union
+from shapely.ops import cascaded_union
+
+
+
+from scipy.spatial import cKDTree
+
 
 #plotly events inside streamlit - https://github.com/null-jones/streamlit-plotly-events
 
@@ -17,6 +24,8 @@ import laspy
 import numpy as np
 import pandas as pd
 from shapely.geometry import Point
+
+from rtree import index
 
 # Roosevelt Island Index = 1625 , 1980
 
@@ -36,7 +45,7 @@ def getLazFile(lazfilename):
         lidarDF['Z'] = lidarDF['Z'] * lz.header.scales[2] + lz.header.offsets[2]
     return lidarDF
 
-def stackTiles_NotLimited(lat,lon, boxSize=1500, prefix ='NY_NewYorkCity/'):  # 'NY_FingerLakes_1_2020/' #
+def stackTiles_NotLimited(lat,lon, boxSize=2000, prefix ='NY_NewYorkCity/'):  # 'NY_FingerLakes_1_2020/' #
     '''
     
     Parameters:
@@ -102,11 +111,11 @@ def stackTiles_NotLimited(lat,lon, boxSize=1500, prefix ='NY_NewYorkCity/'):  # 
             
     lidar_df = lidar_df[lidar_df['Z'] > low ]
     lidar_df = lidar_df[lidar_df['Z'] < high ]
-    lidar_df = lidar_df[lidar_df['X'] <= x + boxSize/2 ]
-    lidar_df = lidar_df[lidar_df['X'] >= x - boxSize/2 ]
-    lidar_df = lidar_df[lidar_df['Y'] <= y + boxSize/2 ]
-    lidar_df = lidar_df[lidar_df['Y'] >= y - boxSize/2 ]
-    return lidar_df
+    # lidar_df = lidar_df[lidar_df['X'] <= x + boxSize/2 ]
+    # lidar_df = lidar_df[lidar_df['X'] >= x - boxSize/2 ]
+    # lidar_df = lidar_df[lidar_df['Y'] <= y + boxSize/2 ]
+    # lidar_df = lidar_df[lidar_df['Y'] >= y - boxSize/2 ]
+    return lidar_df, epsgNumber
 
 def readGeoJSON(filepath):
     with open(filepath) as f:
@@ -122,71 +131,113 @@ def convertXY(x,y):
 ###############################################################################
 
 st.set_page_config(
-        page_title="Roosevelt Island",
+        page_title="Central Park",
 )
 
-st.markdown("<h1 style='text-align: center; color: white;'>Roosevelt Island</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>Central Park</h1>", unsafe_allow_html=True)
 
 
+st.markdown("## Analyzing the Central Park using LiDAR data")
 filepath = '2010 Census Tracts/geo_export_139fc905-b132-4c03-84d5-ae9e70dded42.shp'
 gdf = gpd.read_file(filepath)
 
-# # Create a map of the area
-# selected_points = [
-#   {
-#     "curveNumber": 0,
-#     "pointNumber": 1625, # This is the index of the point in the dataframe
-#     "pointIndex": 1625
-#   }
-# ]
+#rows_to_keep = [1625, 1980]
+Reduced_gdf = gdf.loc[[2150]] # central Park
 
+#Get Bounding Box of Reduced_gdf
+lon_min, lat_min, lon_max, lat_max = Reduced_gdf.total_bounds
 
-# Create a map of the area
-lidarArea = 'NY_NewYorkCity/'
-
-# Get indexs 1625 and 1980 from gdf
-rows_to_keep = [1625, 1980]
-RI_gdf = gdf.loc[rows_to_keep]
-
-#st.write(RI_gdf)
-
-# Get the latitude and longitude of the single row's geometry
-lat, lon = 40.75538269567443, -73.9560580956196 
-
-st.write(f"Location : {lat,lon}")
+# Get the center of the bounding box
+lat = (lat_min + lat_max) / 2
+lon = (lon_min + lon_max) / 2
 
 # Print the latitude and longitude
 print("Latitude:", lat)
 print("Longitude:", lon)
 
-st.write(f"Selected Area : {lidarArea}")
+#st.write(f"Selected Lidar Area : {lidarArea}")
 
 #Plot RI_gdf
+fig = px.choropleth_mapbox(
+        Reduced_gdf,
+        geojson=Reduced_gdf.geometry,
+        locations=Reduced_gdf.index,
+        mapbox_style='carto-positron',
+        center={'lat': lat, 'lon':lon},
+        zoom=12)
 
-# fig = px.choropleth_mapbox(
-#         RI_gdf,
-#         geojson=RI_gdf.geometry,
-#         locations=RI_gdf.index,
-#         mapbox_style='carto-positron',
-#         center={'lat': lat, 'lon': lon},
-#         zoom=12)
+fig.update_layout(height=400, width=600, showlegend=False,
+                margin=dict(l=0,r=0,b=0,t=0),
+                paper_bgcolor="Black"
+)
 
-# fig.update_layout(height=400, width=600, showlegend=False,
-#                 margin=dict(l=0,r=0,b=0,t=0),
-#                 paper_bgcolor="Black"
-# )
+st.plotly_chart(fig)
 
-# st.plotly_chart(fig)
+# vertices = []
+
+# # Check if the geometry is valid
+# if not RI_gdf.geometry.is_valid.any():
+#     st.write("Invalid geometry found, attempting to fix...")
+#     # If the geometry is invalid, buffer it by a small distance to try to fix it
+#     RI_gdf.geometry = RI_gdf.geometry.buffer(0.000001)
+
+# # Iterate over the geometries in the GeoDataFrame
+# for geom in RI_gdf.geometry:
+#     #st.write(geom.geom_type)
+#     # If the geometry is a Polygon, get its vertices
+#     if geom.geom_type == 'Polygon':
+#         vertices.extend(list(geom.exterior.coords))
+#     # If the geometry is a MultiPolygon, get the vertices of its component Polygons
+#     elif geom.geom_type == 'MultiPolygon':
+#         st.write("MultiPolygon")
+#         # for polygon in geom:
+#         #     vertices.extend(list(polygon.exterior.coords))
+
+# st.write(vertices)
+
+
+# # Convert the MultiPolygon to a Polygon
+# merged_geom = unary_union(gdf.geometry)
+
+# # Get the vertices of each geometry in the GeoDataFrame
+# vertices = list(merged_geom.exterior.coords)
+
+# print(vertices)
+
+# Create a map of the area
+lidarArea = 'NY_NewYorkCity/'
+
+st.markdown("## Downloading data from USGS for Central Park")
+
+#Get Bounding Box of RI_gdf
+lon_min, lat_min, lon_max, lat_max = Reduced_gdf.total_bounds
+
+#Print the Bounding Box
+st.write("Bounding Box in lat,long of Central Park:", lat_min, lon_min, lat_max, lon_max)
+
+# Get center of Bounding Box
+lat = (lat_min + lat_max) / 2
+lon = (lon_min + lon_max) / 2
 
 # Get the point cloud data
-Raw_lidar_df = stackTiles_NotLimited(lat,lon,prefix=lidarArea)
+Raw_lidar_df, epsg_no = stackTiles_NotLimited(lat,lon,prefix=lidarArea)
+
+#Convert Bounding Box to epsg_no
+minX, minY = convertLatLon(lat_min, lon_min, epsg_no)
+maxX, maxY = convertLatLon(lat_max, lon_max, epsg_no)
+
+# Print the Bounding Box
+st.write(f"Bounding Box in {epsg_no}:", minX, minY, maxX, maxY)
+#Area of Bounding Box of RI_gdf
+area = (maxX - minX) * (maxY - minY)
+#Print the Area
+st.write("Area of Bounding Box of Central Park:", area)
 
 # Write the number of points found
-
-st.write(f"Totol Number of Points {len(Raw_lidar_df)}")
+st.write(f"Total Number of Raw Points {len(Raw_lidar_df)}")
 
 #make a 3d plot of the points
-Raw_lidar_df = Raw_lidar_df[::2]
+#Raw_lidar_df = Raw_lidar_df[::10]
 
 # Create 3D scatter plot using Plotly with classification-based colors
 fig = px.scatter_3d(Raw_lidar_df, x='X', y='Y', z='Z', color='Z',
@@ -202,11 +253,142 @@ fig.update_yaxes(showticklabels=False)
 
 st.plotly_chart(fig, use_container_width=True)
 
+st.markdown("### Only Central Park Points")
+
+# Get the points within the bounding box from the Raw_lidar_df
+points_in_box = Raw_lidar_df[(Raw_lidar_df['X'] >= minX) & (Raw_lidar_df['X'] <= maxX) & (Raw_lidar_df['Y'] >= minY) & (Raw_lidar_df['Y'] <= maxY)]
+
+# Write the number of points found
+st.write(f"Number of Points in Bounding Box {len(points_in_box)}")
+
+# Create 3D scatter plot using Plotly with classification-based colors
+
+fig = px.scatter_3d(points_in_box, x='X', y='Y', z='Z', color='Z',
+                    hover_data=['X', 'Y', 'Z', 'class'],
+                    color_continuous_scale=px.colors.sequential.Viridis,
+                    width=400, height=500)
+
+fig.update_traces(marker=dict(size=1.2))
+fig.update_layout(scene=dict(aspectmode='data'), showlegend=False)
+fig.update_xaxes(showticklabels=False)
+fig.update_yaxes(showticklabels=False)
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+# Create a KDTree from the points
+#tree = cKDTree(Raw_lidar_df[['X', 'Y']].to_numpy())
+
+# # Get the bounding box of the GeoDataFrame
+# bbox = RI_gdf.total_bounds
+
+# print(bbox)  # (minx, miny, maxx, maxy)
+
+# # Get the bounding box of the polygon
+# lon_min, lat_min, lon_max, lat_max = bbox
+
+# st.write(f"Bounding Box in lat lon : {lon_min, lat_min, lon_max, lat_max}")
+
+# minX, minY = convertLatLon(lat_min, lon_min, epsg_no)
+# maxX, maxY = convertLatLon(lat_max, lon_max, epsg_no)
+
+# st.write(f"Bounding Box in X Y : {minX, minY, maxX, maxY}")
+
+# # Get the points within the bounding box from the Raw_lidar_df
+# points_in_box_df = Raw_lidar_df[(Raw_lidar_df['X'] >= minX) & (Raw_lidar_df['X'] <= maxX) & (Raw_lidar_df['Y'] >= minY) & (Raw_lidar_df['Y'] <= maxY)]
+
+# st.markdown("###Roosevelt Island Points")
+
+# st.write(points_in_box_df)
+
+# # Create 3D scatter plot using Plotly with classification-based colors
+# fig = px.scatter_3d(points_in_box_df, x='X', y='Y', z='Z', color='Z',
+#                     hover_data=['X', 'Y', 'Z', 'class'],
+#                     color_continuous_scale=px.colors.sequential.Viridis,
+#                     width=400, height=500)
+
+# fig.update_traces(marker=dict(size=1.2))
+# fig.update_layout(scene=dict(aspectmode='data'), showlegend=False)
+# fig.update_xaxes(showticklabels=False)
+# fig.update_yaxes(showticklabels=False)
+
+
+# st.plotly_chart(fig, use_container_width=True)
+
+
+#Transform all the points in Raw_lidar_df to lat and lon using apply() and map()
+
+# Raw_lidar_df[['Lat', 'Lon']] = Raw_lidar_df.apply(lambda row: pd.Series(convertXY(row['X'], row['Y'])), axis=1)
+
+# #Create a geometry column from the lat and lon columns
+# Raw_lidar_df['geometry'] = Raw_lidar_df.apply(lambda row: Point(row['Lon'], row['Lat']), axis=1)
+
+# #Create a geodataframe from the lidar dataframe
+# Raw_lidar_gdf = gpd.GeoDataFrame(Raw_lidar_df, geometry='geometry')
+
+# check is Point(X,Y) is within the RI_gdf
+
+# for index, row in Raw_lidar_df.iterrows():
+#     lat, lon = convertXY(row['X'], row['Y'])
+    
+#     polygon = RI_gdf.iloc[0]['geometry']
+
+#     #check if lat and lon are within the polygon
+#     if polygon.contains(Point(lon, lat)):
+#         print("Point is within the polygon")
+#     else:
+#         print("No")
+
+# Create a new dataframe with only the points that are within the RI_gdf
+#RI_lidar_df = gpd.sjoin(Raw_lidar_gdf, RI_gdf, op='within')
+
+# Create 3D scatter plot using Plotly with classification-based colors
+# fig = px.scatter_3d(RI_lidar_df, x='X', y='Y', z='Z', color='Z',
+#                     hover_data=['X', 'Y', 'Z', 'class'],
+#                     color_continuous_scale=px.colors.sequential.Viridis,
+#                     width=400, height=500)
+
+# fig.update_traces(marker=dict(size=1.2))
+# fig.update_layout(scene=dict(aspectmode='data'), showlegend=False)
+# fig.update_xaxes(showticklabels=False)
+# fig.update_yaxes(showticklabels=False)
+
+# Create an R-tree index for the RI_gdf
+# idx = index.Index()
+# for i, geometry in enumerate(RI_gdf.geometry):
+#     idx.insert(i, geometry.bounds)
+
+# # Find the points in Raw_lidar_gdf that fall within the geometries of RI_gdf using the R-tree index
+# hits = []
+# for i, geometry in enumerate(Raw_lidar_gdf.geometry):
+#     for j in idx.intersection(geometry.bounds):
+#         if geometry.within(RI_gdf.iloc[j].geometry):
+#             hits.append(i)
+
+# # Create a new dataframe with only the points that fall within the geometries of RI_gdf
+# boounded_RI_lidar_df = Raw_lidar_gdf.iloc[hits]
+
+# # Create 3D scatter plot using Plotly with classification-based colors
+
+# fig = px.scatter_3d(boounded_RI_lidar_df, x='X', y='Y', z='Z', color='Z',
+#                     hover_data=['X', 'Y', 'Z', 'class'],
+#                     color_continuous_scale=px.colors.sequential.Viridis,
+#                     width=400, height=500)
+
+# fig.update_traces(marker=dict(size=1.2))
+# fig.update_layout(scene=dict(aspectmode='data'), showlegend=False)
+# fig.update_xaxes(showticklabels=False)
+# fig.update_yaxes(showticklabels=False)
+
+# st.plotly_chart(fig, use_container_width=True)
+
+# st.plotly_chart(fig, use_container_width=True)
+
 
 # st.write(RI_new_lidar_df)
 
-# # Perform a spatial join between the two dataframes
-# joined = gpd.sjoin(gpd.GeoDataFrame(RI_lidar_df, geometry=gpd.points_from_xy(RI_lidar_df.X, RI_lidar_df.Y)), RI_gdf, op='within')
+#Perform a spatial join between the two dataframes
+#joined = gpd.sjoin(gpd.GeoDataFrame(RI_lidar_df, geometry=gpd.points_from_xy(RI_lidar_df.X, RI_lidar_df.Y)), RI_gdf, op='within')
 
 # #st.write(joined)
 
